fce76ce9254e4f4550a0d17ac0d737d42481e510
Bruno Fonseca <brunomaiaf20@gmail.com>
2022-04-27 03:34:22 -0400

    validação do forme de serviços

--- app/Models/Servico.php
+++ app/Models/Servico.php
@@ -10,23 +10,23 @@ class Servico extends Model
     use HasFactory;
 
     protected $fillable= [
-        'nome', 
-        'valor_minimo', 
-        'quantidade_horas', 
-        'porcentagem', 
-        'valor_quarto', 
-        'horas_quarto', 
-        'valor_sala', 
+        'nome',
+        'valor_minimo',
+        'quantidade_horas',
+        'porcentagem',
+        'valor_quarto',
+        'horas_quarto',
+        'valor_sala',
         'horas_sala',
         'valor_banheiro',
-        'horas_banheiro', 
-        'valor_cozinha', 
-        'horas_cozinha', 
-        'valor_quintal', 
-        'horas_quintal', 
-        'valor_outros', 
-        'horas_outros', 
-        'icone', 
+        'horas_banheiro',
+        'valor_cozinha',
+        'horas_cozinha',
+        'valor_quintal',
+        'horas_quintal',
+        'valor_outros',
+        'horas_outros',
+        'icone',
         'posicao'
     ];
 }
